0ee913d594f1282c594eada68f4390dbcd144030
azaza12345 <61620351+azaza12345@users.noreply.github.com>
2021-09-30 16:34:33 +0300

    Db connection and project structure

---
+++ csharp-dapper-example/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace csharp_dapper_example.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ csharp-dapper-example/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace csharp_dapper_example.Repository
+{
+    public interface IRepository<T>
+    {
+        void Add(T item);
+        void Delete(int id);
+        void Update(T item);
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+    }
+}
