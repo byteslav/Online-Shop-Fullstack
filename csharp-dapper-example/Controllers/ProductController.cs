854555d5fc2010d27db30a7ad849f8dd1accecec
azaza12345 <61620351+azaza12345@users.noreply.github.com>
2021-09-30 16:54:46 +0300

    Finish controller logic and all views

--- csharp-dapper-example/Controllers/ProductController.cs
+++ csharp-dapper-example/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using csharp_dapper_example.Repository;
+﻿using csharp_dapper_example.Models;
+using csharp_dapper_example.Repository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
@@ -16,6 +17,46 @@ namespace csharp_dapper_example.Controllers
         {
             var items = _productRepository.GetAll();
             return View(items);
+        }
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+ 
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            _productRepository.Add(product);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(int id)
+        {
+            var product = _productRepository.GetById(id);
+            if (product == null) return NotFound();
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _productRepository.Update(product);
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+        
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            _productRepository.Delete(id.Value);
+            return RedirectToAction("Index");
         }
     }
 }
