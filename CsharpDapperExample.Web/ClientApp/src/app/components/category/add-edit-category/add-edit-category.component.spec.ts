1756f98b853569a47d5eb403ce82024350b71239
azaza12345 <61620351+azaza12345@users.noreply.github.com>
2022-02-04 21:08:20 +0300

    complete all fronted tests

--- CsharpDapperExample.Web/ClientApp/src/app/components/category/add-edit-category/add-edit-category.component.spec.ts
+++ CsharpDapperExample.Web/ClientApp/src/app/components/category/add-edit-category/add-edit-category.component.spec.ts
@@ -1,25 +1,51 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddEditCategoryComponent } from './add-edit-category.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {CategoryService} from "../../../services/category.service";
+import {Category} from "../../../models/category";
+import {of} from "rxjs";
 
 describe('AddEditCategoryComponent', () => {
   let component: AddEditCategoryComponent;
   let fixture: ComponentFixture<AddEditCategoryComponent>;
+  let categoryService: CategoryService;
+
+  let mockCategory: Category = { id: 2, name: 'Groceries' };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AddEditCategoryComponent ]
+      imports: [ HttpClientTestingModule ],
+      declarations: [ AddEditCategoryComponent ],
+      providers: [CategoryService]
     })
     .compileComponents();
+
+    categoryService = TestBed.inject(CategoryService);
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AddEditCategoryComponent);
     component = fixture.componentInstance;
+    component.category = mockCategory;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add category', () => {
+    spyOn(categoryService, 'addCategory').and.returnValue(of(mockCategory));
+    component.addCategory();
+
+    expect(component.category).toBe(mockCategory);
+  });
+
+  it('should update category', () => {
+    spyOn(categoryService, 'updateCategory').and.returnValue(of(mockCategory));
+    component.updateCategory();
+
+    expect(component.category).toBe(mockCategory);
   });
 });
--- CsharpDapperExample.Web/ClientApp/src/app/components/product/add-edit-product/add-edit-product.component.spec.ts
+++ CsharpDapperExample.Web/ClientApp/src/app/components/product/add-edit-product/add-edit-product.component.spec.ts
@@ -1,25 +1,58 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddEditProductComponent } from './add-edit-product.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {ProductService} from "../../../services/product.service";
+import {Product} from "../../../models/product";
+import {of} from "rxjs";
+import {Category} from "../../../models/category";
+import {CategoryService} from "../../../services/category.service";
 
 describe('AddEditProductComponent', () => {
   let component: AddEditProductComponent;
   let fixture: ComponentFixture<AddEditProductComponent>;
+  let productService: ProductService;
+  let categoryService: CategoryService;
+
+  let mockProduct: Product = { id: 2, name: 'Bread', price: 50, description: 'Yummy', categoryId: 1, category: {id: 1, name: 'groceries'} };
+  const mockCategories: Category[] = [{ id: 1, name: 'Food' }, {id: 3, name: 'Sport'}];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AddEditProductComponent ]
+      imports: [HttpClientTestingModule],
+      declarations: [ AddEditProductComponent ],
+      providers: [ProductService, CategoryService]
     })
     .compileComponents();
+
+    productService = TestBed.inject(ProductService);
+    categoryService = TestBed.inject(CategoryService);
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AddEditProductComponent);
     component = fixture.componentInstance;
+    component.product = mockProduct;
+
+    spyOn(categoryService, 'getCategoriesList').and.returnValue(of(mockCategories));
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add product', () => {
+    spyOn(productService, 'addProduct').and.returnValue(of(mockProduct));
+    component.addProduct();
+
+    expect(component.product).toBe(mockProduct);
+  });
+
+  it('should update product', () => {
+    spyOn(productService, 'updateProduct').and.returnValue(of(mockProduct));
+    component.updateProduct();
+
+    expect(component.product).toBe(mockProduct);
   });
 });
